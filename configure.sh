6b6849917b9f0342490f54989aa4e1ca790ac21a
Archit Dey <deyarchit@Archits-MacBook-Air.local>
2024-11-08 00:05:08 -0800

    Customizing configure script

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# # Install brew
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Updating package lists..."
 brew update
@@ -13,8 +13,8 @@ brew install zsh zsh-completions
 
 # Installing git completion
 echo ''
-echo "Now installing git and bash-completion..."
-brew install git && brew install bash-completion
+echo "Now installing bash-completion..."
+brew install bash-completion
 
 echo ''
 echo "Now configuring git-completion..."
@@ -40,30 +40,30 @@ git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugi
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
-# powerlevel9k install
-echo ''
-echo "Now installing powerlevel9k..."
-echo ''
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# # powerlevel9k install
+# echo ''
+# echo "Now installing powerlevel9k..."
+# echo ''
+# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# vimrc vundle install
-echo ''
-echo "Now installing vundle..."
-echo ''
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# # vimrc vundle install
+# echo ''
+# echo "Now installing vundle..."
+# echo ''
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Pathogen install
-echo ''
-echo "Now installing Pathogen..."
-echo ''
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# # Pathogen install
+# echo ''
+# echo "Now installing Pathogen..."
+# echo ''
+# mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+# 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# Nerdtree for vim install
-echo ''
-echo "Now installing Nerdtree for Vim..."
-echo ''
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+# # Nerdtree for vim install
+# echo ''
+# echo "Now installing Nerdtree for Vim..."
+# echo ''
+# git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 # Vim color scheme install
 echo ''
@@ -72,49 +72,49 @@ echo ''
 git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
 mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
 
-# Midnight commander install
-echo ''
-echo "Now installing Midnight commander..."
-echo ''
-brew install mc
+# # Midnight commander install
+# echo ''
+# echo "Now installing Midnight commander..."
+# echo ''
+# brew install mc
 
-# Speedtest-cli and jq install
-brew install jq speedtest-cli
+# # Speedtest-cli and jq install
+# brew install jq speedtest-cli
 
-# Pull down personal dotfiles
-echo ''
-read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo ''
-	echo "Now pulling down jldeen dotfiles..."
-	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
-	echo ''
-	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
-	echo ''
-	echo "Checking out macOS branch..." && git checkout mac
-	echo ''
-	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
-    if [[ $? -eq 0 ]]
-    then
-        echo "Successfully configured your environment with jldeen's macOS dotfiles..."
-    else
-        echo "jldeen's macOS dotfiles were not applied successfully..." >&2
-fi
-else 
-	echo ''
-    echo "You chose not to apply jldeen's macOS dotfiles. You will need to configure your environment manually..."
-	echo ''
-	echo "Setting defaults for .zshrc and .bashrc..."
-	echo ''
-	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
-	echo ''
-	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
-	echo ''
-	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
+# # Pull down personal dotfiles
+# echo ''
+# read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
+# echo    # (optional) move to a new line
+# if [[ $REPLY =~ ^[Yy]$ ]]
+# then
+#     echo ''
+# 	echo "Now pulling down jldeen dotfiles..."
+# 	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
+# 	echo ''
+# 	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
+# 	echo ''
+# 	echo "Checking out macOS branch..." && git checkout mac
+# 	echo ''
+# 	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+#     if [[ $? -eq 0 ]]
+#     then
+#         echo "Successfully configured your environment with jldeen's macOS dotfiles..."
+#     else
+#         echo "jldeen's macOS dotfiles were not applied successfully..." >&2
+# fi
+# else 
+# 	echo ''
+#     echo "You chose not to apply jldeen's macOS dotfiles. You will need to configure your environment manually..."
+# 	echo ''
+# 	echo "Setting defaults for .zshrc and .bashrc..."
+# 	echo ''
+# 	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
+# 	echo ''
+# 	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
+# 	echo ''
+# 	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
 	
-fi
+# fi
 
 # Set default shell to zsh
 echo ''
